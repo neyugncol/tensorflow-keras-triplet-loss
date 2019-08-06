4b76275874518dd4a66a6c1d05a3a70bb5348126
ndloc867 <ndloc867@gmail.com>
2019-08-06 10:45:10 +0700

    FIX: param name mismatch

--- utils/image_processing.py
+++ utils/image_processing.py
@@ -51,7 +51,7 @@ def get_confusion_matrix_figure(confusion_matrix, labels):
     confusion_matrix = confusion_matrix.astype(np.float) / confusion_matrix.sum(axis=1)[:, np.newaxis]
     df = pd.DataFrame(confusion_matrix, labels, labels)
     ax = sns.heatmap(df, cmap=plt.cm.Blues, cbar=False)
-    ax.set_xticklabels(ax.get_xticklabels(), rotate=45)
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
     ax.set(xlabel='Predicted label', ylabel='True label', title='Confusion matrix')
 
     return ax.get_figure()
